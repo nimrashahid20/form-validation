9bb8869842d2e7b7dd0d7d4cab70fbaa28de8de3
nimrashahid20 <nimrashahid02007@gmail.com>
2024-09-11 15:46:24 +0500

    form

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+// console.log("Java Script")
+//  function addition(){
+//     console.log("hello World")
+// }
+// addition()
+
+// try{
+//     console.log("Nimra")
+//     Registration()
+// }
+// // catch{
+// //     console.log("funcion is calling is not exist")
+// // }
+// catch(err){
+// console.log(err)
+// }
+
+// function greatWorld(){
+//     try{
+//         let great = "hello World";
+//         aler(great)
+//         // throw "syntax Error"
+//     }
+//     catch(err){
+//         console.log(err)
+
+//     }
+// }
+// greatWorld()
+
+
+// function checkPassword(){
+//     try{
+//         let pass = document.getElementById("f1")
+//         if(pass.lenght<8){
+//           throw "please enter atleast 8 characters "
+
+//         }
+//         if(pass.indexOf(" ")){
+//             throw "no s paces in the password, please"
+//         }
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+// }
+
+
+
+
+
+
